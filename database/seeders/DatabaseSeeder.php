f078df82da8cc2f16099353b4d20824a754fdce1
Osatsu025 <osatsu025@gmail.com>
2024-09-10 11:22:10 +0900

    fix: dankaのサンプルデータを作成

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,9 +16,9 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
         //\App\Models\Danka::truncate();
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        \App\Models\User::truncate();
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
-        // $this->call(DankasTableSeeder::class);
+        //DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        //\App\Models\User::truncate();
+        //DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        $this->call(DankasTableSeeder::class);
     }
 }
